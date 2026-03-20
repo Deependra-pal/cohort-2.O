16434d0ca7b6f9d454aff4d3159ecdd2ff6abef0
Deependra Pal <deependrap412@gmail.com>
2026-03-20 23:11:05 +0530

    day-7

---
+++ day-7/src/app.js
@@ -0,0 +1,50 @@
+const express  = require('express');
+const noteModel = require('./models/notes.model');
+
+
+const app =  express();
+
+app.use(express.json());
+
+
+// POST/notes
+
+
+app.post('/notes', async (req,res)=>{
+    const {title,description} = req.body
+
+   const note =  await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message:"Note created sucessfully",
+        note
+    })
+})
+
+// GET/notes
+
+ app.get('/notes', async (req,res)=>{
+
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        meassage:"Fetch data",
+        notes
+    })
+
+
+      
+ })
+
+
+
+
+
+module.exports = app;
+
+
+
+
+
