feed1c048542b48cbb02b95df934dd6e132de3d0
Deependra Pal <deependrap412@gmail.com>
2026-03-27 21:02:26 +0530

    feat: initialize day-13 project with Express, MongoDB, and user authentication
    - created package.json with necessary dependencies
    - implemented server.js to set up the Express server
    - added app.js to configure middleware and routes
    - established database connection in database.js
    - defined user model in user.model.js
    - created auth.routes.js for user registration and login functionality

---
+++ day-13/src/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: {
+        type:String,
+        unique: [true, "with this email user account already exits"]
+    },
+    password: String
+})
+
+
+const userModel = mongoose.model('users', userSchema);
+
+
+module.exports = userModel 
---
+++ day-13/src/routes/auth.routes.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const userModel = require("../models/user.model");
+const authRouter = express.Router();
+const JWT = require("jsonwebtoken");
+const crypto = require('crypto')
+
+authRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashPassword =  crypto.createHash('md5').update(password).digest('hex')
+
+  const isUserAlreadyExits = await userModel.findOne({ email });
+
+  if (isUserAlreadyExits) {
+    return res.status(409).json({
+      message: "user already  exits with this email address",
+    });
+  }
+
+  const user = await userModel.create({
+    name,
+    email,
+    password:hashPassword,
+  });
+
+  const token = JWT.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("JWT_token", token);
+
+  res.status(201).json({
+    message: "user register sucessfully",
+    user,
+    token,
+  });
+});
+
+authRouter.post("/protected", (req, res) => {
+  console.log(req.cookies);
+});
+
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found with this email address",
+    });
+  }
+
+  const isPasswordMatch = user.password === crypto.createHash('md5').update(password).digest("hex")
+
+  if (!isPasswordMatch) {
+    return res.status(401).json({
+      message: "Invalid Password",
+    });
+  }
+
+  const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User logged in",
+    user,
+    token,
+  });
+});
+
+module.exports = authRouter;
