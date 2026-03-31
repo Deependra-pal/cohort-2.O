6c8d7a5d25cb4b202a16c0f52ec2b14ad3f05b4d
Deependra Pal <deependrap412@gmail.com>
2026-03-31 23:23:05 +0530

    fix: remove duplicate cookie-parser middleware usage in app.js

--- day-15/src/app.js
+++ day-15/src/app.js
@@ -6,10 +6,10 @@ const cookieParser = require("cookie-parser");
 
 const app = express()
 
-
+app.use(cookieParser())
 app.use(express.json())
 app.use('/api/auth' , authRouter);
-app.use(cookieParser())
+ 
  
  
 
